2bf1dc192cfcafc45f70d326b5465207c2abe113
Tan Grumser <tan.grumser@gmail.com>
2021-01-24 22:20:06 +0100

    Cleaned up code, added .exe.

--- GridPuzzle.cpp
+++ GridPuzzle.cpp
@@ -47,14 +47,14 @@ bool simulateWalkPath(int *aField) {
         field[i] = aField[i];
     }
 
-    while (steps < 400) { // As long as the position hasn't been reached before.
+    while (steps < 400) { // Just walk for some time to see if every field was visited.
         // Move to next field and mark old field as already visited.
         field[position] = 2;
         int oldPosition = position;
 
         switch (direction) {
             case Direction:: UP: 
-                if (position < SIZE_X) { // if we're in the upper row.
+                if (position < SIZE_X) { // If we're in the upper row.
                     rotate(&direction);
                 } else {
                     position -= SIZE_X;
@@ -62,7 +62,7 @@ bool simulateWalkPath(int *aField) {
             break;
 
             case Direction::RIGHT: 
-                if (position % SIZE_X == SIZE_X - 1) { // if we're in the right collumn.
+                if (position % SIZE_X == SIZE_X - 1) { // If we're in the right collumn.
                     rotate(&direction);
                 } else {
                     position++;
@@ -70,7 +70,7 @@ bool simulateWalkPath(int *aField) {
             break;
 
             case Direction::LEFT: 
-                if (position % 5 == 0) { // if we're in the right collumn.
+                if (position % 5 == 0) { // If we're in the left collumn.
                     rotate(&direction);
                 } else {
                     position--;
@@ -78,7 +78,7 @@ bool simulateWalkPath(int *aField) {
             break;
 
             case Direction::DOWN: 
-                if (position >= SIZE_X * (SIZE_Y - 1)) { // if we're in the lowest row.
+                if (position >= SIZE_X * (SIZE_Y - 1)) { // If we're in the lowest row.
                     rotate(&direction);
                 } else {
                     position += SIZE_X;
@@ -86,7 +86,7 @@ bool simulateWalkPath(int *aField) {
             break;
         }
 
-        if (field[position] == 1) { // if we ran into a boulder.
+        if (field[position] == 1) { // If we ran into a boulder.
             position = oldPosition;
             rotate(&direction);
         } else {
@@ -95,7 +95,7 @@ bool simulateWalkPath(int *aField) {
     }
     
     for (int i = 0; i < SIZE_X * SIZE_Y; i++) {
-        if (field[i] == 0) { // if theres a empty spot that we haven't reached.
+        if (field[i] == 0) { // If theres a empty spot that we haven't reached.
             return false;
         }
     }
@@ -109,13 +109,13 @@ bool findSolution (int *field, int numberOfRocks) {
     }
 
     for (int i = 0; i < SIZE_X * SIZE_Y; i++) {
-        if (field[i] == 0 && i != STARTING_POSITION) { // if we're at and empty spot that's not the starting position.
-            field[i] = 1; // we place a boulder.
+        if (field[i] == 0 && i != STARTING_POSITION) { // If we're at and empty spot that's not the starting position.
+            field[i] = 1; // We place a boulder.
 
-            if (findSolution(field, numberOfRocks - 1)) { // a solution was found.
+            if (findSolution(field, numberOfRocks - 1)) { // A solution was found.
                 return true;
             } else {
-                field[i] = 0; // otherwise we remove the bolder to place it on another field.
+                field[i] = 0; // Otherwise we remove the bolder to place it on another field.
 
             }
         } else {
@@ -127,8 +127,11 @@ bool findSolution (int *field, int numberOfRocks) {
 }
 
 int main() {
-    const int numberOfRocks = 4;
+    int numberOfRocks;
     int *field = (int*) malloc (sizeof(int) * SIZE_X * SIZE_Y);
+    
+    cout << "Enter the number of rocks to place: ";
+    cin >> numberOfRocks;
 
     for (int i = 0; i < SIZE_X * SIZE_Y; i++) {
         field[i] = 0; 
